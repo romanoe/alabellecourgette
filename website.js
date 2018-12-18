45ea77724ea7f87afe0bfdb792b296849b8a6606
romanoe <noemi.romano@protonmail.com>
2018-12-18 14:55:35 +0100

    deploy

--- website.js
+++ website.js
@@ -38,6 +38,10 @@ app.use("*",function(req,res){
   res.sendFile(path + "404.html");
 });
 
-app.listen(3000,function(){
-  console.log("Live at Port 3000");
+
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
 });
