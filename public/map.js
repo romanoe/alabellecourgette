0ec96f251848d690f59cc40f1213f6e1e88916d5
Noemi Romano <romanoe@lasigadmin-128.178.82.208>
2019-02-28 16:27:24 +0100

    Version 1

--- public/map.js
+++ public/map.js
@@ -4,15 +4,13 @@ var courgette = L.icon({
     iconUrl: '/images/courgette1.svg',
 
     iconSize:     [38, 95], // size of the icon
-   
-   
+
+
 });
 
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 L.marker([46.560167, 6.554194], {icon: courgette}).addTo(map);
-
-
