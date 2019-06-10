861defc8459c58a5134f731522e52dedf705f5d7
Arthur Turin <info@alabellecourgette.ch>
2019-06-10 13:40:40 +0200

    dernières grosses modifs

--- public/map.js
+++ public/map.js
@@ -1,16 +1,33 @@
-var map = L.map('map').setView([46.560167, 6.554194], 13);
+var map = L.map('map').setView([46.538518, 6.593337], 12);
 
-var courgette = L.icon({
-    iconUrl: '/images/courgette1.svg',
+var pelle = L.icon({
+    iconUrl: '/images/pelle_cut.png',
 
-    iconSize:     [38, 95], // size of the icon
+    iconSize:     [52, 40], // size of the icon
 
 
 });
+
+var courgette = L.icon({
+    iconUrl: '/images/courgette_logo2.png',
+
+    iconSize:     [52, 40], // size of the icon
+
+
+});
+
+// var courgette = L.icon({
+//     iconUrl: '/images/logo_map.png',
+//
+//     iconSize:     [52, 40], // size of the icon
+//
+//
+// });
 
 
 L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([46.560167, 6.554194], {icon: courgette}).addTo(map);
+L.marker([46.560167, 6.554194], {icon: pelle}).addTo(map);
+L.marker([46.521749, 6.632884], {icon: courgette}).addTo(map);
